121b971b1e40d02f068f4a044527536cd05d5970
galaxy-tek <550935327@qq.com>
2023-09-14 08:06:11 +0800

    new env test

--- first_script.m
+++ first_script.m
@@ -1,10 +1,10 @@
-% for i=1:10
-%     x = linspace(0,10,101);
-%     plot(x, sin(x+i));
-%     print(gcf,'-deps',strcat('plot', num2str(i),'.ps'));
-% end
+for i=1:10
+    x = linspace(0,10,101);
+    plot(x, sin(x+i));
+    print(gcf,'-deps',strcat('plot', num2str(i),'.ps'));
+end
 
 %% this is a new section
 
-x = input('please input x :');
+%x = input('please input x :');
 
